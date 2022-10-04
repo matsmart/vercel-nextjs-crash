e30a5207b384833a472af44304bf07dee21fbac7
simon.johansson <simonljohansson92@gmail.com>
2022-10-04 17:24:08 +0200

    cleanup

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 ## Reproducing
 
 1. Clone the repository
-2. Run `yarn dlx vercel pull`
-3. Create a new project on your user account through the CLI
+3. Create a new project on your user account through the vercel CLI or through your vercel account
 4. Configure the project to use root directory "app", framework preset "nextjs". Overwrite "build command" to "(:)" and "install command" to "(:)" (without the quotes)
-5. Create a PR towards main
+5. Setup github secrets `VERCEL_TOKEN` with a token scoped for your account, `VERCEL_PROJECT_ID` with the newly created project id and `VERCEL_ORD_ID` with your organization id
+6. Create a PR towards main with an arbitrary change or push an empty commit to main
+7. Open the preview url and notice that it crashes.
--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,20 +1,8 @@
-// import bundleAnalyzer from '@next/bundle-analyzer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
-
-// const withBundleAnalyzer = bundleAnalyzer({
-//   enabled: false,
-//   openAnalyzer: false,
-// })
-
-// const withTranspileModules = transpileModules([
-//   '@findify/sdk',
-//   '@findify/analytics',
-//   '@findify/change-emitter',
-// ])
 
 /**
  * @type {import('next').NextConfig}
@@ -33,32 +21,6 @@ const config = {
       },
     ],
     formats: ['image/avif', 'image/webp'],
-  },
-  async headers() {
-    return [{ source: '/:path*', headers: [
-      {
-        key: 'X-DNS-Prefetch-Control',
-        value: 'off',
-      },
-      {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=63072000; includeSubDomains; preload',
-      },
-      {
-        key: 'X-XSS-Protection',
-        value: '1; mode=block',
-      },
-      // Disable X-Frame-Options until we have fixed Formstack integration.
-      //
-      // {
-      //   key: 'X-Frame-Options',
-      //   value: 'SAMEORIGIN',
-      // },
-      {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff',
-      },
-    ] }]
   },
   webpack: (config) => {
     return config
